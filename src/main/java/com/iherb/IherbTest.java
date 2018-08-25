2778b4be78bd9bed30b63f4e12cb5b7d83dc4e33
maria_shukhter <maria.shukhter@icloud.com>
2018-08-25 16:01:15 -0700

    Initial  commit

---
+++ src/main/java/com/iherb/IherbTest.java
@@ -0,0 +1,61 @@
+package com.iherb;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+import java.util.concurrent.TimeUnit;
+
+public class IherbTest {
+
+   @Test
+
+   public void addProductToCartTest(){
+       System.setProperty("webdriver.gecko.driver", "/Users/maria_shukhter/Desktop/selenium");
+       System.setProperty("webdriver.chrome.driver", "/Users/maria_shukhter/Desktop/selenium/chromedriver");
+
+       WebDriver driver = new ChromeDriver();
+
+       driver.navigate().to("https://www.iherb.com/");
+       Assert.assertTrue("tile should start with iHerb.com",
+              driver.getTitle().startsWith("iHerb.com"));
+
+       System.out.println("The website is opened");
+
+       driver.navigate().to("https://www.iherb.com/c/supplements");
+       Assert.assertTrue("title should start with Supplements",
+               driver.getTitle().startsWith("Supplements"));
+
+       System.out.println("The page is opened");
+
+       ((ChromeDriver) driver).findElementByCssSelector("a[href='https://www.iherb.com/pr/California-Gold-Nutrition-Omega-3-Premium-Fish-Oil-100-Fish-Gelatin-Softgels/62118']").click();
+
+       ((ChromeDriver) driver).findElementByCssSelector("button[data-product-id='62118']").click();
+       driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+       System.out.println("The product in the cart");
+
+
+      ((ChromeDriver) driver).findElementsByCssSelector("a[href='https://checkout.iherb.com/EditCart']").get(1).click();
+
+       System.out.println("The card is opened");
+
+
+       Boolean isPresent = ((ChromeDriver) driver).findElementsByXPath("//*[contains(text(), 'MLI-00952')]").size()>0;
+       Assert.assertTrue(isPresent);
+
+       System.out.println("The product is found in the cart");
+
+
+       driver.close();
+       driver.quit();
+
+
+   }
+
+
+}
